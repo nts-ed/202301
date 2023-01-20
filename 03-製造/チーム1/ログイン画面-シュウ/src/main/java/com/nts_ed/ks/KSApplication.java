11be4d2d6c7368b3b0b6931a973dcc948cf2520d
LAPTOP-NMI6E175\Administrator <tingna1993@gmail.com>
2023-01-20 17:26:10 +0900

    no message

---
+++ 03-製造/チーム1/ログイン画面-シュウ/src/main/java/com/nts_ed/ks/KSApplication.java
@@ -0,0 +1,13 @@
+package com.nts_ed.ks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KSApplication.class, args);
+	}
+
+}
