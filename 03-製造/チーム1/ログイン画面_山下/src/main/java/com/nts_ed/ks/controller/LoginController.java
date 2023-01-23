53bcc77e605eec4d06b704f13f768730c8465455
takafumi yamashita <>
2023-01-23 15:50:46 +0900

    ログイン画面_山下

---
+++ 03-製造/チーム1/ログイン画面_山下/src/main/java/com/nts_ed/ks/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.nts_ed.ks.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nts_ed.ks.dto.Employee;
+import com.nts_ed.ks.param.LoginParam;
+import com.nts_ed.ks.repository.EmployeeRepository;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/")
+	public String index(LoginForm form, Model model) {
+		model.addAttribute("title", "勤怠管理システム");
+		return "index";
+	}
+
+	@PostMapping("/login")
+	public String login(LoginParam param, @Validated LoginForm form,  BindingResult result, Model model) {
+		// 社員のチェックを行います
+		List<Employee> isExists = employeeRepository.checkEmployee(param.getId(), param.getPass());
+		if (isExists.size() == 1) {
+			model.addAttribute("loginList", isExists);
+			model.addAttribute("title", "勤怠管理ログイン成功");
+			return "login/index"; //ログイン成功時ここを変更する
+		}
+		//フォームに入力誤りがあった場合
+		if(result.hasErrors()) {
+			model.addAttribute("title", "勤怠管理システム");
+			return "index";
+		}
+		return "index";
+	}
+
+	
+}
---
+++ 03-製造/チーム1/ログイン画面_山下/src/main/java/com/nts_ed/ks/param/LoginParam.java
@@ -0,0 +1,12 @@
+package com.nts_ed.ks.param;
+
+import lombok.Data;
+
+@Data
+public class LoginParam {
+
+	private String id;
+	
+	private String pass;
+	
+}
